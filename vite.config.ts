beeda482391d4ddcade050a27a43acf6bd211d5c
Riccio <riccio.fernando@gmail.com>
2025-10-19 01:32:30 -0300

    adicionar url do azure

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
 
   preview: {
     host: '0.0.0.0',
-    port: 8080
+    port: 8080,
+    allowedHosts: ["cineai-cgdyhggmhjgacab4.brazilsouth-01.azurewebsites.net"],
   },
 })
