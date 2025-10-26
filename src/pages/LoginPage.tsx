3da23ab9379b7047fa155faa93a1dc8405166509
Riccio <166852556+Ricciow@users.noreply.github.com>
2025-10-26 12:12:57 -0300

    ajustes na pagina de login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,13 @@
 import { Form } from "react-router-dom";
 import FormField from "../components/Form/FormField";
+import FormTitle from "../components/Form/FormTitle";
 
 export default function LoginPage() {
     return (
         <div className="login_main"> 
             <h1 className="cine_ai_title">CineAI</h1>
             <Form method="post" className="login_form">
+                <FormTitle title="Bem-vindo de volta!" description="Acesse sua conta para continuar"/>
                 <FormField type="email" name="email" placeholder="Email" title="Email" required value=""/>
                 <FormField type="password" name="password" placeholder="Senha" title="Senha" required value=""/>
                 <FormField type="submit" name="submit" placeholder="" required value="Entrar"/>
